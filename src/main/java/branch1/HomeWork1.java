8cbef6eee0985abc75cab94873bb33ce5a9b6f23
Александр Паршуков <parshukov.oboi@gmail.com>
2022-11-15 13:08:28 +0500

    first

---
+++ src/main/java/branch1/HomeWork1.java
@@ -0,0 +1,10 @@
+package branch1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+    }
+    private static final void getText(){
+        System.out.println("hello word!");
+    }
+}
